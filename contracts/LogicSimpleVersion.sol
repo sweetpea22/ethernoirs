6de5b2c691fdff6c96a64fa329e4fbd3e60058b2
sweetpea22 <cindychau24@me.com>
2023-09-23 18:23:49 -0400

    Test new way out

--- contracts/LogicSimpleVersion.sol
+++ contracts/LogicSimpleVersion.sol
@@ -1,22 +1,31 @@
 pragma solidity ^0.8.18;
+import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract Game {
-    address public owner;
+contract SimpleLogic is Ownable {
     uint256 public numberOfLevels;
     
     mapping(uint256 => mapping(address => uint256)) public playerChoices;
 
     constructor(uint256 _numberOfLevels) {
-        owner = msg.sender;
         numberOfLevels = _numberOfLevels;
     }
 
-    function makeChoice(uint256 level, uint256 choice) external public {
+    function setNumberOfLevels(uint256 _numberOfLevels) public onlyOwner {
+        numberOfLevels = _numberOfLevels;
+    }
+
+    function makeChoice(uint256 level, uint256 choice) external {
         require(level < numberOfLevels, "Invalid level");
         require(choice < 256, "Choice must be between 0 and 255");
         
         playerChoices[level][msg.sender] = choice;
     }
+
+   function getChoice(uint256 level, address player) public view returns (uint256) {
+    require(level < numberOfLevels, "Invalid level");
+    return playerChoices[level][player];
+}
+
 }
 
 
